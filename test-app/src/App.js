86ebcdda9fdb1f8477728be96d113d2163b60068
Sumit <sumitsaurabh9868@gmail.com>
2020-10-12 00:00:58 +0530

    first commit

---
+++ test-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import MyResume from './MyResume'
+function App() {
+  return (
+    <div>
+    <div className="app">
+    <div className="myrow">
+     <h1>abc</h1>
+     <h2>efg</h2>
+     </div>
+    </div>
+    <MyResume/>
+    </div>
+  );
+}
+
+export default App;
---
+++ test-app/src/MyResume.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function MyResume() {
+    return(<div>
+        <div className="app">
+        <div className="myrow">
+         <h1>abc</h1>
+         <h2>efg</h2>
+         </div>
+        </div>
+        <MyResume/>
+        </div>);
+}
+
+export default MyResume;
